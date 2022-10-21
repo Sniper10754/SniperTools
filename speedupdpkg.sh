a3e396ec580beb842d9daf65de7d16bd6159fd35
Sniper10754 <74132494+Sniper10754@users.noreply.github.com>
2022-10-21 21:59:39 +0200

    Create speedupdpkg.sh

---
+++ speedupdpkg.sh
@@ -0,0 +1,6 @@
+dpkg --clear-avail
+
+#dpkg --forget-old-unavail
+# Nowadays deprecated.
+
+echo If you use grep-available or other tools that rely on a useful available file, update the available file using sync-available
